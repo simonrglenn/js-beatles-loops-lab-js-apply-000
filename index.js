b51f5ec58105deee3e016136620c1e5947b90976
Simon Glenn <simonrglenn@gmail.com>
2019-05-29 00:27:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const theBeatlesPlay = (musicians, instruments) => {
   return beatlesArr
   }
 
-const johnLennonFacts = facts => {
-  let j = 0;
-  let wowFacts = [];
-  while (j < facts.length) {
-    wowFacts.push(facts[j] + '!!!');
-    ++j;
+  function johnLennonFacts() {
+      var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+      var newFacts = [];
+      var i = 0;
+      while (i < facts.length) {
+          newFacts.push(facts[i] +"!!!");
+          i++;
+      }
+      return newFacts;
   }
-  return facts;
-}
